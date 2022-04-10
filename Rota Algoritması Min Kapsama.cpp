bf7390dcfdad18cec723545467af7575a80b7b39
Mehmet Emin Ak <76618785+MehmetEminAk@users.noreply.github.com>
2022-04-10 15:32:33 +0300

    Update Rota Algoritması Min Kapsama.cpp

--- Rota Algoritması Min Kapsama.cpp
+++ Rota Algoritması Min Kapsama.cpp
@@ -16,18 +16,18 @@ int main()
     setlocale(LC_ALL,"Turkish");
     cout << "      **********Rota Algoritması  Hesaplayan Program**********\n";
     cout << "LüTFEN KAÇA KAÇLIK BİR MATRİS ÜZERİNDE İŞLEM YAPILACAĞINI GİRİNİZ:";
-    cin >> a;
-    cin >>b;
+    cin >> a;						//kullanıcıdan matrisin satır sayısını alma
+    cin >>b;						//kullanıcıdan matrisin sütun sayısını alma
     
 
-    int matrix[a][b];
-    int temp;
+    int matrix[a][b];					//matrisi oluşturduk	
+    int temp;						//elemanlar eklenmeden önce geçici olarak temp'e yüklenicek,temp belirli şartları sağlarsa matrise işlenicek.
     for (int i = 0; i < a; i++) {
         for (int j = 0; j < b; j++) {
         	
             cout << "Lütfen matrisinizin " << i+1 << ". satırdaki ve " << j+1 << ". sütundaki elemanını giriniz";
             cin>>temp;
-            if(temp==0 || temp==1){
+            if(temp==0 || temp==1){               //Girilen sayının 1 veya 0 olup olmadığını kontrol etme.
             	matrix[i][j]=temp;
 			}
 			else{
@@ -42,31 +42,31 @@ int main()
 		exit;
 		
     
-    for (int k = 0; k < a; k++) {
+    for (int k = 0; k < a; k++) {			//matrisi konsol ekranına yazdıracak
         for (int l = 0; l < b; l++) {
             cout << matrix[k][l]<<"\t";
             
         }
         cout << "\n";
     }
-    int sutunAgirliklari[b];
+    int sutunAgirliklari[b];				//sütun ağırlıkları diye yeni bir liste tanımladık.
 	for(int o=0;o<b;o++){
-		sutunAgirliklari[o]=0;
+		sutunAgirliklari[o]=0;			//ve ilk etapta tüm elemanlarını sıfırladık.
 	}
 	for(int m=0;m<a;m++){
 		for(int n=0;n<b;n++){
-			if(matrix[m][n]==1){
+			if(matrix[m][n]==1){		//her bir hücreyi kontrol ediyoruz ve 1 değerinde olan hücrenin sütun ağırlığını 1 arttırıyoruz
 				sutunAgirliklari[n]++;
 			}
 			else{
-				continue;
+				continue;		
 			}
 		}
 	}
 	float satirAgirliklari[a];
 	for(int p=0;p<b;p++){
-		cout<<p+1<<". sütunun agirligi"<<sutunAgirliklari[p]<<"\n ";
-		if(sutunAgirliklari[p]==1){
+		cout<<p+1<<". sütunun agirligi"<<sutunAgirliklari[p]<<"\n ";    //Her bir sütunun ağırlığını yazdırıyoruz ve eğer mutlak satır varsa yani ağırlığı
+		if(sutunAgirliklari[p]==1){								//1 olan sütun varsa belirtiyoruz.
 			cout<<"bundan dolayı "<<p+1<<". satır mutlak satırdır. \n";
 		}
 		
@@ -74,18 +74,20 @@ int main()
 	}
 	
 	for(int i=0;i<a;i++){
-		satirAgirliklari[i]=0.00;   //Satır ağırlıklarını tutan dizinin her bir elemanını sıfırladım.
+		satirAgirliklari[i]=0;   //Satır ağırlıklarını tutan dizinin her bir elemanını sıfırladık.
 	}
 	for(int m=0;m<a;m++){
 		
 		for(int n=0;n<b;n++){
 			if(matrix[m][n]==1){
 				
-				satirAgirliklari[m]+=float((1/sutunAgirliklari[n]));
+				satirAgirliklari[m]+=(1/sutunAgirliklari[n]);
 			}
 	
 }
+satirAgirliklari[m]*=a;
 }
+
 for(int g=0;g<a;g++){
 	cout<<satirAgirliklari[g]<<"\n";
 }
